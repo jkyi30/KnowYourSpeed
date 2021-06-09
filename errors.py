6b3110bc0ac6600f5f0afbe2c7dbe01892598f9e
justine <78173200+jkyi30@users.noreply.github.com>
2021-06-09 11:10:39 -0400

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,28 @@
+#This calculates number of wrong words
+def error (orig, typeinput):
+    n = 0
+    a = 0
+    errors = 0
+    err = ''
+    ti = typeinput.split()
+    orig1 = orig.split()
+    while n < len(ti):
+         if n < len(orig1):
+            if ti[n] != orig1[a]:
+                errors += 1
+                if len(ti[n]) >= len(orig1[n]) + len(orig1[n+1]):
+                    a += 1
+         n += 1
+         a += 1
+    errors += abs(len(orig1) - len(ti))
+    return errors
+  
+# Accuracy based on words total and words correct  
+def accuracy (orig, Typeinput):
+    incorrect = error (orig, Typeinput)
+    correct = len(orig.split()) - incorrect
+    acc = correct/ len(orig.split()) 
+    acc = acc * 100
+    return str(acc) + '%'
+  
+  
